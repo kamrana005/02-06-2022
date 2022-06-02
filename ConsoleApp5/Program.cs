4cff92f17ed2afc2af07376f2e87e573091ebc65
kamrana005 <kamranda@gmail.edu.az>
2022-06-02 06:49:48 +0400

    file created

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("username");
+            string us = Console.ReadLine();
+
+            User user = new User(us);
+
+            
+            
+
+            Console.WriteLine("password");
+            user.Password = Console.ReadLine();
+            
+
+             
+           
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp5/User.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    internal class User
+    {
+
+        public User(string us)
+        {
+            UserName = UserName;
+        }
+        private string _userName;
+        private string _password;
+
+
+
+
+       
+
+        public string UserName
+        {
+
+            get
+            {
+                return _userName;
+               
+            }
+            set
+            {
+                for(int i = 0; i < value.Length; i++)
+                {
+                    if (value.Length > 5 && value.Length < 25)
+                    {
+                        _userName = value;
+                    }
+
+                }
+                
+
+            }
+
+
+
+             
+                    
+                    
+                        
+                    
+                
+                
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value) && (value.Length > 5 && value.Length < 25) && HasUpper(value) && HasLower(value) && IsDigit(value)) ;
+                _password = value;
+
+
+
+
+            }
+
+
+
+
+
+        }
+        static bool HasUpper(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (Char.IsUpper(value[i]))
+                    return true;
+            }
+            return false;
+        }
+        static bool HasLower(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (Char.IsLower(value[i]))
+                    return true;
+            }
+            return false;
+        }
+
+
+        static bool IsDigit(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (Char.IsDigit(value[i]))
+                    return true;
+            }
+            return false;
+
+
+        }
+
+        
+
+        
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    
+}
